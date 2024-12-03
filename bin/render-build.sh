1a9ad748128dac5e5afb0238bba64b2e458b72d6
Harugo Shintani <shin@shin-MBA.local>
2024-12-03 16:08:22 +0900

    set port 3000

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,11 @@
-# docker用のビルドスクリプト
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+# # docker用のビルドスクリプト
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
 
-# 必要なDocker操作を記述
-docker-compose run --rm web bundle install
-docker-compose run --rm web bundle exec rails assets:precompile
-docker-compose run --rm web bundle exec rails assets:clean
-docker-compose run --rm web bundle exec rails db:create     # データベース作成
-docker-compose run --rm web bundle exec rails db:migrate
+# # 必要なDocker操作を記述
+# docker-compose run --rm web bundle install
+# docker-compose run --rm web bundle exec rails assets:precompile
+# docker-compose run --rm web bundle exec rails assets:clean
+# docker-compose run --rm web bundle exec rails db:create     # データベース作成
+# docker-compose run --rm web bundle exec rails db:migrate
